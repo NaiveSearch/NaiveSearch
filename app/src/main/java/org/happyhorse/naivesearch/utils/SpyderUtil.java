1aeb44ab91b10314d2a705128eb7dd7d8267bf46
whonamedlalala <56581472+whonamedlalala@users.noreply.github.com>
2021-12-11 20:43:57 +0800

    Improve Web Crawler to bypass the Baidu detection
    RT

--- app/src/main/java/org/happyhorse/naivesearch/utils/SpyderUtil.java
+++ app/src/main/java/org/happyhorse/naivesearch/utils/SpyderUtil.java
@@ -4,24 +4,24 @@ import org.happyhorse.naivesearch.config.Config;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 /**
  * This class support to get a unprocessed request results with the {@link SpyderUtil#getSearchResult(int, java.lang.String, int)} API.
  * The results are requesting with particular cookies and User Agent
  */
 public class SpyderUtil {
-    static boolean init = false;    //is need to initial
+    //is need to initial
+    static boolean init = false;
 
     //store the cookies of supported search engines
     static Map<String, String> cookies, baidu_cookie, bing_cookie;
+
 
     /**
      * Return the html stream according to given search engine, key word and page number
@@ -35,35 +35,40 @@ public class SpyderUtil {
      */
     public static String getSearchResult(int engine, String keyword, int page) throws IOException {
         //do initialization if not initial before
-        if (!init) {
+        if(!init){
             init();
-            init = true;
-        }
-
-        //choose what cookies will be attached based on given search engine
-        if (engine == Config.BAIDU_SEARCH) {
-            cookies = baidu_cookie; //cookies for Baidu engine
-        } else if (engine == Config.BING_SEARCH) {
-            cookies = bing_cookie;  //cookies for Bing engine
+            init=true;
         }
 
         //build a particular URL for request
-        String url = urlMaker(engine, keyword, page);
+        String url = urlMaker(engine,keyword,page);
+        Document document = null;
+        //choose what header will be attached based on given search engine
+        if (engine == Config.BAIDU_SEARCH) {
+            cookies = baidu_cookie; //cookies for Baidu engine
+            document = requestBaidu(url);
+        } else if (engine == Config.BING_SEARCH) {
+            cookies = bing_cookie;  //cookies for Bing engine
+            document = requestBing(url);
+        }
 
-        //get the request results and return
-        Document document = request(url);
         return document.toString();
     }
+
 
     /**
      * Initialize all cookies for supported search engines using {@link Jsoup#connect(java.lang.String)}
      *
      * @throws IOException come from {@link Connection#execute()}
      */
-    private static void init() throws IOException {
+    public static void init() throws IOException {
         //get cookies for Baidu engine
         Connection conn = Jsoup.connect(Config.SEARCH_ENGINE_URL[Config.BAIDU_SEARCH]);
-        Connection.Response res = conn.method(Connection.Method.POST).timeout(3000).execute();
+        Connection.Response res = conn.method(Connection.Method.POST).timeout(3000)
+                .header("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                .header("Accept-Encoding","gzip, deflate, br")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36")
+                .execute();
         baidu_cookie = res.cookies();
 
         //get cookies for Bing engine
@@ -71,6 +76,7 @@ public class SpyderUtil {
         Connection.Response res2 = conn2.method(Connection.Method.POST).timeout(3000).execute();
         bing_cookie = res2.cookies();
     }
+
 
     /**
      * Return the results of request the url
@@ -82,37 +88,25 @@ public class SpyderUtil {
      * @throws IOException come from {@link Connection#get()}
      * @see Jsoup
      */
-    private static Document request(String url) throws IOException {
-        //User Agent pool
-        List<String> ualist = Arrays.asList(
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1 BingPreview/1.0b",
-                "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1 BingPreview/1.0b",
-                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3 BingPreview/1.0b",
-                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24 BingPreview/1.0b",
-                "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24 BingPreview/1.0b");
+    public static Document requestBaidu(String url) throws IOException {
+        Connection connect = Jsoup.connect(url);
 
-        //get a User Agent from pool
-        Random rand = new Random();
-        //String ua = ualist.get(rand.nextInt(ualist.size()));//TODO:not used
-        String ua = ualist.get(2);
+        Document document = connect
+                .header("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                .header("Accept-Encoding","gzip, deflate, br")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36")
+                .header("Referer","https://www.baidu.com/")
+                .timeout(3000)
+                .ignoreContentType(true)
+                .cookies(cookies)
+                .get();
+        return document;
+    }
 
-        //request the url with selected User Agent and cookies, then return the request results
+    public static Document requestBing(String url) throws  IOException{
         Connection connect = Jsoup.connect(url);
         Document document = connect
-                .userAgent(ua)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36")
                 .timeout(3000)
                 .ignoreContentType(true)
                 .cookies(cookies)
